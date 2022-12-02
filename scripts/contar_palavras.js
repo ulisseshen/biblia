27539332da355e701391476d038439e4e903a505
Ulisses Hen <ulisses.hen@hotmail.com>
2022-12-02 20:46:52 -0300

    criado script para contar palavras.

---
+++ scripts/contar_palavras.js
@@ -0,0 +1,42 @@
+// script usado para contar as palavra nos livros da Biblia.
+// script feitos no navegador.
+
+let bible = JSON.parse(document.body.innerText)
+let livros = bible.map(book => book.chapters.join(' ')
+                       .replace(/"/gi,"")
+                       .replace(/,,/gm, " ")
+                       .replace(/\.,/gi," ")
+                       .replace(/\:,/gm, " ")
+                       .replace(/\:/gm, "")
+                       .replace(/\. /gm, " ")
+                       .replace(/\, /gm, " ")
+                       .replace(/\; /gm, " ")
+                       .replace(/\! /gm, " ")
+                       .replace(/\? /gm, " ")
+                       .replace(/\, /gm, " ")
+                       .replace(/ \,/gm, " ")
+                       .replace(/\./gm, "")
+                       .replace(/  /gm, " ")
+                       .replace(/\,/gm, " ")
+                       .replace(/ ‘/gm, " ")
+                       .replace(/ ’ /gm, " ")
+                       .replace(/!/gm, " ")
+                      )
+
+let livroCompleto = livros.join(' ').replace(/  /gm, " ");
+let palavras = livroCompleto.split(' ');
+
+let quantidadePalavras = palavras.reduce(function (todosPalavras, palavra) {
+  if (palavra in todosPalavras) {
+    todosPalavras[palavra]++;
+  }
+  else {
+    todosPalavras[palavra] = 1;
+  }
+  return todosPalavras;
+}, {});
+
+let stringGroup = JSON.stringify(quantidadePalavras, null, null);
+
+console.log(stringGroup)
+
