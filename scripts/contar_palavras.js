fbd1497b9702a0a8dc76a4322b24eb913517043e
Ulisses Hen <ulisses.hen@hotmail.com>
2022-12-06 21:41:44 -0300

    add regex para palavras em português.

--- scripts/contar_palavras.js
+++ scripts/contar_palavras.js
@@ -1,5 +1,8 @@
 // script usado para contar as palavra nos livros da Biblia.
 // script feitos no navegador.
+let regexs = {
+  palavraPortugues: /[a-zA-Zà-úÀ-Ú]+/gmi
+}
 
 let bible = JSON.parse(document.body.innerText)
 let livros = bible.map(book => book.chapters.join(' ')
